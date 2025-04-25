12a0ea37505f3f9598ad39d918f51aa11e727050
Ian <iankruze60@gmail.com>
2025-04-25 22:35:24 +0300

    Adds modified app.js and app.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,26 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
-import '@testing-library/jest-dom';
+describe("Initial State", () => {
+  // ... (previous tests)
+});
 
-// Code tests here
+describe("Checkbox Interaction", () => {
+  // ... (previous tests)
+
+  test("selected topping disappears when checked a second time", () => {
+    render(<App />);
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+    userEvent.click(addPepperoni);
+    expect(addPepperoni).toBeChecked();
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.getByText("Pepperoni")).toBeInTheDocument();
+
+    userEvent.click(addPepperoni);
+    expect(addPepperoni).not.toBeChecked();
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument();
+  });
+});
